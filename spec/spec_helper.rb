4a0cc15f3c362207893cca509e7184e9c2f079ee
Brett Huneycutt <brett@1000memories.com>
2012-12-12 13:01:41 -0800

    make testing nicer

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,3 +4,10 @@ Bundler.require :default, ENV['RACK_ENV'].to_sym
 
 require "./lib/photo"
 require "./lib/quilt"
+
+RSpec.configure do |config|
+  config.treat_symbols_as_metadata_keys_with_true_values = true
+
+  config.run_all_when_everything_filtered = true
+  config.filter_run :focus
+end
